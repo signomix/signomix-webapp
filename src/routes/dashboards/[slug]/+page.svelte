706426550eae0b06e12558672797612d84f25440
Grzegorz Skorupa <g.skorupa@gmail.com>
2023-05-18 21:53:13 +0200

    svelte-grid

--- src/routes/dashboards/[slug]/+page.svelte
+++ src/routes/dashboards/[slug]/+page.svelte
@@ -1,17 +1,20 @@
+<!--
+    see: https://svelte-grid.vercel.app/examples/responsive
+-->
 <div
     class="component d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h2>{data.name}</h2><a href="/dashboards/{data.id}/edit">Configure</a>
 </div>
-<div class="component">
-    <div class="row">
-        
-        {#each data.widgets as widget}
-        <div class="col-{widget.width} p-2 m-1 border border-primary">{widget.title}</div>
-        {/each}
-        
-    </div>
+<div class="demo-container size">
+    <Grid bind:items={items} rowHeight={100} let:item {cols} let:index>
+        <div class="demo-widget content">
+            <h5>widget {index}</h5>
+        </div>
+    </Grid>
 </div>
 <script>
+    import Grid from "svelte-grid";
+    import gridHelp from "svelte-grid/build/helper/index.mjs";
     import { dev } from '$app/environment';
     import { utils } from '$lib/utils.js';
     import { goto } from '$app/navigation';
@@ -24,68 +27,73 @@
     userSession.subscribe(value => {
         session = value;
     });
+    
+    const { item } = gridHelp;
+    const id = () => "_" + Math.random().toString(36).substr(2, 9);
 
-    function doSend(event) {
+    let editable = true; // set to true to enable editing
 
-        let dataToSend = {}
-        let method = 'POST'
-        if (event.target.elements['id'].value.length > 0) {
-            dataToSend.id = event.target.elements['id'].value
-        }
-        dataToSend.description = event.target.elements['description'].value
-        dataToSend.created = utils.getDateApiFormat(event.target.elements['createdAt'].value)
-        dataToSend.updated = event.target.elements['updatedAt'].value
-        if (dataToSend.updated.length > 0) {
-            dataToSend.updated = utils.getDateApiFormat(dataToSend.updated)
-        }
+    let items = [
+        {
+            id: id(),
+            10: item({
+                x: 0,
+                y: 0,
+                w: 2,
+                h: 2,
+                draggable: editable, resizable: editable,
+            }),
+            6: item({ x: 0, w: 2, h: 2, y: 0, draggable: editable, resizable: editable, }),
+            1: item({ x: 0, y: 0, w: 1, h: 2, draggable: editable, resizable: editable, }),
+        },
+        {
+            id: id(),
+            10: item({
+                x: 2,
+                y: 0,
+                w: 3,
+                h: 2,
+            }),
+            6: item({ x: 2, w: 1, h: 2, y: 0 }),
+            1: item({ x: 0, y: 2, w: 1, h: 2 }),
+        },
 
-        const headers = new Headers()
-        let url = utils.getBackendUrl(location) + '/api/dashboards/'
-        if (!(dataToSend.id == null || dataToSend.id == '' || dataToSend.id == undefined)) {
-            url = url + dataToSend.id
-            method = 'PUT'
-        }
-        if (!isValid(dataToSend)) {
-            return
-        }
-        headers.set('Authorization', 'Basic ' + btoa(session.login + ":" + session.password));
-        headers.set('Content-Type', 'application/json')
-        //console.log('URL:' + url)
-        let response = fetch(
-            url,
-            { method: method, mode: 'cors', headers: headers, body: JSON.stringify(dataToSend) }
-        ).then((response) => {
-            if (response.status == 200) {
-                errorMessage = ''
-                goto('/configs')
-            } else if (response.status == 401 || response.status == 403 || response.status == 404) {
-                utils.setAuthorized(session, false)
-            } else {
-                alert(
-                    utils.getMessage(utils.FETCH_STATUS)
-                        .replace('%1', response.status)
-                        .replace('%2', response.statusText)
-                )
-            }
-        }).catch((error) => {
-            errorMessage = error.message
-            if (errorMessage == 'Failed to fetch' && location.protocol.toLowerCase() == 'https') {
-                errorMessage = errorMessage + ' Możliwa przyczyna: self signed nie są obsługiwane.'
-            }
-            console.log(error)
-        });
-    }
+        {
+            id: id(),
+            10: item({
+                x: 0,
+                y: 2,
+                w: 5,
+                h: 2,
+            }),
+            6: item({ x: 0, w: 3, h: 2, y: 2 }),
+            1: item({ x: 0, y: 4, w: 1, h: 2 }),
+        },
+    ];
 
-    function isValid(data) {
-
-        return true
-    }
-
-    function cancel() {
-        goto('/dashboards')
-    }
-    function clearMessage() {
-        errorMessage = ''
-    }
-
+    const cols = [
+        [1500, 10],
+        [1024, 6],
+        [500, 1],
+    ];
 </script>
+<style>
+    .size {
+        max-width: 1500px;
+        width: 100%;
+    }
+
+    .demo-widget {
+        background: #f1f1f1;
+        height: 100%;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .demo-container {
+        max-width: 1500px;
+        width: 100%;
+    }
+</style>
